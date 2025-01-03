20435696e550318ddc65f003640c2ad82b45112b
JSancho <von.sancho@gmail.com>
2025-01-03 12:22:35 +0100

    Los botones cambian de color. Añado Favicon. Elimino style.css.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function newMonthHtml(currentMonth, currentYear) {
+    let ciudad;
     const monthBlock = document.createElement("div");
     monthBlock.classList.add("month-block");
     monthBlock.classList.add("border");
@@ -29,25 +30,40 @@ document.addEventListener("DOMContentLoaded", () => {
     monthBlock.innerHTML = `
       <h2>${months[currentMonth]} ${currentYear}</h2>
       <div class="line-item border p-2">
-        <span>Update PDF #1</span>
+        <span>${ciudad = "Zaragoza"}</span>
         <div>
-          <button type="button" id="Luz${months[currentMonth]}${currentYear}" class="btn btn-primary btn-sm">Luz &nbsp;<i class="fa-regular fa-lightbulb"></i></button>
-          <button type="button" id="Gas${months[currentMonth]}${currentYear}" class="btn btn-primary btn-sm">Gas &nbsp;<i class="fa-solid fa-fire"></i></button>
-          <button type="button" id="Agua${months[currentMonth]}${currentYear}" class="btn btn-primary btn-sm">Aguas &nbsp;<i class="fa-solid fa-droplet"></i></button>
-          
-          
+          <button type="button" name="botonConfirmacion" id="Luz-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Luz &nbsp;<i class="fa-regular fa-lightbulb"></i></button>
+          <button type="button" name="botonConfirmacion" id="Gas-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Gas &nbsp;<i class="fa-solid fa-fire"></i></button>
+          <button type="button" name="botonConfirmacion" id="Agua-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Aguas &nbsp;<i class="fa-solid fa-droplet"></i></button>
+        </div>
+      </div>
+
+      <div class="line-item border p-2">
+        <span>${ciudad = "Madrid"}</span>
+        <div>
+          <button type="button" name="botonConfirmacion" id="Luz-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Luz &nbsp;<i class="fa-regular fa-lightbulb"></i></button>
+          <button type="button" name="botonConfirmacion" id="Gas-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Gas &nbsp;<i class="fa-solid fa-fire"></i></button>
+          <button type="button" name="botonConfirmacion" id="Agua-${months[currentMonth]}-${currentYear}-${ciudad}" class="btn btn-primary btn-sm">Aguas &nbsp;<i class="fa-solid fa-droplet"></i></button>
         </div>
       </div>
     `;
     monthList.appendChild(monthBlock);
   }
 
-  // Event delegation for activating icons
-  monthList.addEventListener("click", (event) => {
-    if (event.target.tagName === "I") {
-      event.target.classList.toggle("active");
-    }
-  });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const botonesConfirmacion = document.getElementsByName("botonConfirmacion");
+  console.log(botonesConfirmacion);
+  botonesConfirmacion.forEach((boton) => {
+    boton.addEventListener("click", function () {
+      if (this.classList.contains("btn-primary")) {
+        this.classList.replace("btn-primary", "btn-success");
+      } else if (this.classList.contains("btn-success")) {
+        this.classList.replace("btn-success", "btn-primary");
+      }
+    });
+  });
+})
 
 
